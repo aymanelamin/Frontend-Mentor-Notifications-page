7efa9816b6bbce0ea62be89c394755003b54e44f
aymanelamin <aymanelamin06@gmail.com>
2024-02-28 08:54:12 +0200

    first submmit

---
+++ main.js
@@ -0,0 +1,12 @@
+
+function markAsRaid() {
+    document.getElementById("unreadIcon1").style.display = "none";
+    document.getElementById("unreadIcon2").style.display = "none";
+    document.getElementById("unreadIcon3").style.display = "none";
+
+    document.getElementById("notification1").style.backgroundColor = "white";
+    document.getElementById("notification2").style.backgroundColor = "white";
+    document.getElementById("notification3").style.backgroundColor = "white";
+
+}
+
